a551b90ed5077e9a779cfd2b10668f9c462628ce
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-06-27 16:02:16 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	char ch;
+	int A, B;
+	while (cin >> A >> B)
+	{
+
+		
+		
+		cout << A + B << endl;
+		if (!EOF)
+			break;
+	}
+	return 0;
+}
+
