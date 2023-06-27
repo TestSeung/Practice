96e5d2a15b21ba0302e53a29c15328072c432c82
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-06-27 15:52:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(void)
+{
+	int N;
+	std::cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
