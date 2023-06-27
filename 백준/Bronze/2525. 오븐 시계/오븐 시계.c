2f5c9e3f70ea1d57df6f5505bd27985b3dffc924
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-06-27 15:52:50 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int c;
+    scanf("%d",&c);
+    
+    if(b+c<60)
+        printf("%d %d",a, b+c);
+    else{
+        int hour = (b+c)/60;
+        int min = (b+c)%60;
+        if(a+hour<24)
+            printf("%d %d",a+hour,min);
+        else
+            printf("%d %d",a+hour-24,min);
+    }
+    return 0;
+}
