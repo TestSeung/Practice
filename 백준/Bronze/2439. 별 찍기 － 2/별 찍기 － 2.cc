856df5f7c50b3792bf4241c8f0e5fa382ab86f12
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-06-27 15:52:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		
+		for (int y = 0; y < N-i; y++)
+		{	
+			
+			cout << " ";
+
+			
+		}
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
