777712b15eb3cb3e2796e0ae61451de9eca7d16b
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-06-28 09:51:45 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -1,41 +1,34 @@
 #include<iostream>
 using namespace std;
 
+void Gimozzy(int i,int N)
+{
+	for (int j = N; i < j - 1; j--)
+	{
+		cout << " ";
+	}
+	for (int j = 0; j <= i; j++)
+	{
+		cout << "*";
+	}
+	for (int j = 1; j <= i; j++)
+	{
+		cout << "*";
+	}
+	cout << "\n";
+}
+
 int main()
 {
 	int N;
 	cin >> N;
 	for (int i = 0; i < N-1; i++)
 	{
-		for (int j = N; i < j-1; j--)
-		{
-			cout << " ";
-		}
-		for (int j = 0; j <= i; j++)
-		{
-			cout << "*";
-		}
-		for (int j = 1; j <= i; j++)
-		{
-			cout << "*";
-		}
-		cout << "\n";
+		Gimozzy(i, N);
 	}
 	for (int i = N-1; 0 <= i; i--)
 	{
-		for (int j = N; i < j-1; j--)
-		{
-			cout << " ";
-		}
-		for (int j = 0; j <= i; j++)
-		{
-			cout << "*";
-		}
-		for (int j = 1; j <= i; j++)
-		{
-			cout << "*";
-		}
-		cout << "\n";
+		Gimozzy(i, N);
 	}
 	return 0;
 }
