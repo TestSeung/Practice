4c265e4c10f17692b33fe50953c68f9f573a7a3f
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-06-29 13:51:57 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+
+	int len = str.length();
+
+	for (int i=0;i<len/2;i++)
+	{
+		if (str[i] != str[str.length() - 1 - i])
+		{
+			cout << "0";
+			return 0;
+		}
+	}
+		cout << "1";
+}
