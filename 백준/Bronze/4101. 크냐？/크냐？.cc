5ae83aef3e01b2c09fa8f80efa67ca3c7446fdc4
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-07-04 11:37:30 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.cc
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -5,13 +5,12 @@ using namespace std;
 int main()
 {
 	int Num1, Num2;
-	bool Loop = true;
-	while (Loop)
+	while (true)
 	{
 		cin >> Num1>>Num2;
 		if(Num1==0&&Num2==0)
 		{
-			Loop = false;
+			break;
 		}
 		else if (Num1 > Num2)
 		{
