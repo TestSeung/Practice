185c9c0edcf4c8171d38a0ffc7844a088e55cd1b
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-07-04 10:55:57 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N[3];
+	for(int i=0;i<3;i++)
+	cin >> N[i];
+
+	sort(N, N+3);
+	
+	for (int i = 0; i < 3; i++)
+	cout << N[i]<<" ";
+
+	return 0;
+}
