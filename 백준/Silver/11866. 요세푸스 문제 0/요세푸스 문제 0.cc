8a1defd06ad2dac8ea60af980d06513ba7d72bbb
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-08-19 21:29:42 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	queue<int>q;
+	int N, K;
+
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++)
+	{
+		q.push(i);
+	}
+	cout << "<";
+	while (q.size()!=0)
+	{
+		for (int i = 1; i < K; i++)
+		{
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+		if (q.size() != 1)
+		{
+			cout << ", ";
+		}
+		q.pop();
+	}
+	cout << ">";
+	return 0;
+}
+
