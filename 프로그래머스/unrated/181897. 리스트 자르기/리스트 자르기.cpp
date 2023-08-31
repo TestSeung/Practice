1cfa60020463218b77dcf7afd229def845d9cc46
TestSeung <107861712+TestSeung@users.noreply.github.com>
2023-08-31 20:19:46 +0900

    [unrated] Title: 리스트 자르기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.cpp
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.cpp
@@ -3,25 +3,30 @@
 
 using namespace std;
 
+
+
 vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
     vector<int> answer;
+    int a = slicer[0];
+    int b = slicer[1];
+    int c = slicer[2];
     switch (n)
     {
         case 1:
-            for(int i =0;i<=slicer[1];i++)
+            for(int i =0;i<=b;i++)
                 
                 answer.push_back(num_list[i]);
             break;
         case 2:
-            for(int i =slicer[0];i<num_list.size();i++)
+            for(int i =a;i<num_list.size();i++)
                 answer.push_back(num_list[i]);
             break;
         case 3:
-            for(int i =slicer[0];i<=slicer[1];i++)
+            for(int i =a;i<=b;i++)
                 answer.push_back(num_list[i]);
             break;
         case 4:
-            for(int i =slicer[0];i<=slicer[1];i+=slicer[2])
+            for(int i =a;i<=b;i+=c)
                 answer.push_back(num_list[i]);
 
             break;
