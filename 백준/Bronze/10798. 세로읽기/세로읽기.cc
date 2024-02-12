6aa2aea3d1546a7673e774be52dc73892b7bb409
TestSeung <107861712+TestSeung@users.noreply.github.com>
2024-02-12 19:08:32 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -7,22 +7,19 @@ int main() {
 
 	string s;
 	vector<string>v;
-	int temp = 0;
-
 
 	for (int i = 0; i < 5; i++)
 	{
 		cin >> s;
 		v.push_back(s);
-		if (temp < s.size())
-			temp = s.size();
-	}
 
-	for (int i = 0; i < temp; i++)
+	}
+	for (int i = 0; i < 15; i++)
 	{
 		for (int j = 0; j < v.size(); j++)
 			if (i < v[j].size())
 				cout << v[j][i];
 
 	}
+	return 0;
 }
