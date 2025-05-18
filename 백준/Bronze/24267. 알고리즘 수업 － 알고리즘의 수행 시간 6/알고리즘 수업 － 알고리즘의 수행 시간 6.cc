477e93a28b5f6aa4cf87377483f5c0a7df5e3719
TestSeung <107861712+TestSeung@users.noreply.github.com>
2025-05-18 22:47:19 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	long long num;
+	long long result = 0;
+	cin >> num;
+	result =(num -2)* (num - 1)* num / 6;
+	
+	cout << result << '\n';
+
+	cout << 3;
+}
