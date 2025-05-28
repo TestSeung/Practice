c2b425dd84d9aa569593c025346d303ac1dc50f7
TestSeung <107861712+TestSeung@users.noreply.github.com>
2025-05-28 18:55:40 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -2,35 +2,32 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
 	int t,num;
-	int q =0, d =0, n =0, p = 0;
+	int q =0, d =0, n =0;
 
 	cin >> t;
 
 	while (t--) {
 		cin >> num;
-		while (num != 0) {
-			if (num - 25 >= 0) {
-				num -= 25;
-				q++;
-			}
-			else if (num - 10 >= 0) {
-				num -= 10;
-				d++;
-			}
-			else if (num - 5 >= 0) {
-				num -= 5;
-				n++;
-			}
-			else if (num - 1 >= 0) {
-				num -= 1;
-				p++;
-			}
+		
+		if (num % 25 >= 0&&num>=25) {
+			q = num / 25;
+			num -= 25 * q;
 		}
-		printf("%d %d %d %d\n", q, d, n, p);
-		q = 0, d = 0, n = 0, p = 0;
+		if (num % 10 >= 0 && num >= 10) {
+			d = num / 10;
+			num -= 10 * d;
+		}
+		if (num % 5 >= 0 && num >= 5) {
+			n = num / 5;
+			num -= 5 * n;
+		}
+		
+		printf("%d %d %d %d\n", q, d, n, num);
+		q = 0, d = 0, n = 0;
+	
 	}
 	
 }
