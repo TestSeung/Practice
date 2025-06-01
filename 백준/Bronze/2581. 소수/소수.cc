07b29e0796dd80f78043ed746282a4a1c6487424
TestSeung <107861712+TestSeung@users.noreply.github.com>
2025-06-01 19:19:06 +0900

    [Bronze II] Title: 소수, Time: 132 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+void prime(int min, int max);
+
+int main() {
+	int num1,num2;
+	int cnt = 0;
+
+	cin >> num1 >> num2;
+
+	prime(num1, num2);
+	
+}
+
+
+void prime(int min,int max) {
+
+	int cnt = 0;
+	int sum = 0;
+	deque<int> d;
+
+	for (int i = min;i <= max;i++) {
+
+		if (i == 1) continue;
+
+		for (int j = 1;j < i;j++) {
+			if (i % j == 0) {
+				cnt++;
+			}
+		}
+		
+		if (cnt == 1) {
+			
+			d.push_back(i);
+			sum += i;
+		}
+		
+		cnt = 0;
+	}
+	if (d.empty()) {
+		cout << -1;
+		return;
+	}
+
+	cout << sum << '\n' << d.front();
+	
+
+}
