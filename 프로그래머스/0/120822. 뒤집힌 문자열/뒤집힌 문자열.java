5bdd494c1f88a90d459b3670b7cacc2b1a801269
TestSeung <107861712+TestSeung@users.noreply.github.com>
2026-04-03 15:15:32 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,12 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String my_string) {
-        
-        StringBuilder sb = new StringBuilder(my_string);
-        
-        sb.reverse();
-        
-        return sb.toString();
+    public String solution(String my_string) {   
+        return new StringBuilder(my_string).reverse().toString();
     }
 }
