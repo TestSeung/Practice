17ffa49cf4e22b89b307d3edc0a2100c91b640d8
TestSeung <107861712+TestSeung@users.noreply.github.com>
2026-04-04 23:58:59 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.09 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String my_string) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(char c: my_string.toCharArray()){
+            if(c>='0'&&c<='9'){
+                answer.add(c-'0');
+            }
+        }
+
+        Collections.sort(answer);
+        
+        return answer;
+    }
+}
