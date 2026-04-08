6113f47ad822ac4c9bebb9f4e8b2286a516b90e2
TestSeung <107861712+TestSeung@users.noreply.github.com>
2026-04-08 23:52:48 +0900

    [level 0] Title: 영어가 싫어요, Time: 9.08 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/README.md
+++ 프로그래머스/0/120894. 영어가 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 영어가 싫어요 - 120894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894) 
 
 ### 성능 요약
 
-메모리: 84.5 MB, 시간: 2.50 ms
+메모리: 87.2 MB, 시간: 9.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 08일 23:45:05
+2026년 04월 08일 23:52:45
 
 ### 문제 설명
 
--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,42 +1,12 @@
 class Solution {
     public long solution(String numbers) {
 
-        String[] alphabet = {"one","two","three","four","five","six","seven","eight","nine","zero"};
+        String[] alphabet = {"zero","one","two","three","four","five","six","seven","eight","nine"};
         
-        String union = "";
-        String result = "";
-        for(String s : numbers.split("")){
-            union += s;
-            for(String alpha : alphabet){
-                if(union.equals(alpha)){
-                    if(alpha.equals("one")){
-                        result += "1";
-                    }else if(alpha.equals("two")){
-                        result += "2";
-                    }else if(alpha.equals("three")){
-                        result += "3";
-                    }else if(alpha.equals("four")){
-                        result += "4";
-                    }else if(alpha.equals("five")){
-                        result += "5";
-                    }else if(alpha.equals("six")){
-                        result += "6";
-                    }else if(alpha.equals("seven")){
-                        result += "7";
-                    }else if(alpha.equals("eight")){
-                        result += "8";
-                    }else if(alpha.equals("nine")){
-                        result += "9";
-                    }else if(alpha.equals("zero")){
-                        result += "0";
-                    }
-                    
-                    union = "";
-                }
-            }
-            
-        }
+       for(int i=0;i<alphabet.length;i++){
+           numbers = numbers.replace(alphabet[i],i+"");
+       }
        
-        return Long.parseLong(result);
+        return Long.parseLong(numbers);
     }
 }
