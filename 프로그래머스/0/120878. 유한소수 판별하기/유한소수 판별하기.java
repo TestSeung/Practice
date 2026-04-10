35b65017dd050098fd5c8b0a6aab3f5706687182
TestSeung <107861712+TestSeung@users.noreply.github.com>
2026-04-10 22:24:50 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 1;
+        int num =0;
+        num = gcd(a,b);
+        
+        b/=num;
+        
+        while(b!=1){
+            if(b%2==0){
+                b/=2;
+            }
+            else if(b%5==0){
+                b/=5;
+            }
+            else{
+                return 2;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public static int gcd(int a,int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+}
