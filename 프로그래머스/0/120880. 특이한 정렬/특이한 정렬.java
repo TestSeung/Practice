59c6b924d37998002a224f2908d4618a25f671b9
TestSeung <107861712+TestSeung@users.noreply.github.com>
2026-04-10 23:52:58 +0900

    [level 0] Title: 특이한 정렬, Time: 0.69 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = new int[numlist.length];
+        
+        int[][] length = new int[numlist.length][2];
+        
+        for(int i =0;i<numlist.length;i++){
+            length[i][0] = Math.abs(n-numlist[i]);
+            length[i][1] = Math.abs(numlist[i]);
+        }
+        
+        Arrays.sort(length,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                return o1[0]!=o2[0] ? o1[0]-o2[0]:o2[1]-o1[1];
+            }
+        });
+                    
+        for(int i =0;i<answer.length;i++){
+            answer[i] = length[i][1];
+        }
+        
+        
+        return answer;
+    }
+}
