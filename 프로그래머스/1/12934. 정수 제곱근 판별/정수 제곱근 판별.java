cec20dc97f65407f7804c84031ce6dc6d3f09d59
TestSeung <107861712+TestSeung@users.noreply.github.com>
2026-04-14 21:25:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 12.01 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+
+        for(long i = 1;i<=n;i++){
+            if(Math.sqrt(n)==i){
+                return (i+1)*(i+1);
+            }
+        }
+        
+        return -1;
+    }
+}
